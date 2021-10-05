b969e9c58cb926f57003fcbc1446efc261e7687b
Angelo <angelotremoureux.f2f@gmail.com>
2021-10-05 12:12:37 +0200

    Début des tests

--- ClassesMetier/Prestation.cs
+++ ClassesMetier/Prestation.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Prestation.cs" company="Angelo">
+﻿// <copyright file="Prestation.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
