36206c2a7d89a7ea74ab4690b4f6de1b8db449bf
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-10-31 10:05:13 +0100

    Ajout d'une fonctionnalité : cotation

---
+++ ClassesMetier/Cotation.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="Cotation.cs" company="Angelo">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabinetMedical.ClassesMetier
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Cotation d'une prestation.
+    /// </summary>
+    public class Cotation
+    {
+        private int id;
+        private string libelle;
+        private double indice;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cotation"/> class.
+        /// </summary>
+        /// <param name="id">Id.</param>
+        /// <param name="libelle">Libellé.</param>
+        /// <param name="indice">Valeur.</param>
+        public Cotation(int id, string libelle, double indice)
+        {
+            this.id = id;
+            this.libelle = libelle;
+            this.indice = indice;
+        }
+
+        /// <summary>
+        /// Gets id.
+        /// </summary>
+        public int Id { get => this.id; }
+
+        /// <summary>
+        /// Gets libelle.
+        /// </summary>
+        public string Libelle { get => this.libelle; }
+
+        /// <summary>
+        /// Gets or sets prize.
+        /// </summary>
+        public double Indice { get => this.indice; set => this.indice = value; }
+    }
+}
--- ClassesMetier/Prestation.cs
+++ ClassesMetier/Prestation.cs
@@ -19,6 +19,7 @@ namespace CabinetMedical.ClassesMetier
         private string libelle;
         private DateTime dateHeureSoin;
         private Intervenant intervenant;
+        private Dictionary<Cotation, int> cotations;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Prestation"/> class.
@@ -26,7 +27,8 @@ namespace CabinetMedical.ClassesMetier
         /// <param name="libelle">Libellé.</param>
         /// <param name="dateHeureSoin">Date du soin.</param>
         /// <param name="intervenant">Intervenant.</param>
-        public Prestation(string libelle, DateTime dateHeureSoin, Intervenant intervenant)
+        /// <param name="cotations">Cotations.</param>
+        public Prestation(string libelle, DateTime dateHeureSoin, Intervenant intervenant, Dictionary<Cotation, int> cotations)
         {
             if (dateHeureSoin > DateTime.Now)
             {
@@ -39,6 +41,32 @@ namespace CabinetMedical.ClassesMetier
 
             this.libelle = libelle;
             this.intervenant = intervenant;
+            this.cotations = cotations;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Prestation"/> class.
+        /// </summary>
+        /// <param name="libelle">Libellé.</param>
+        /// <param name="dateHeureSoin">Date du soin.</param>
+        /// <param name="intervenant">Intervenant.</param>
+        public Prestation(string libelle, DateTime dateHeureSoin, Intervenant intervenant)
+            : this(libelle, dateHeureSoin, intervenant, new Dictionary<Cotation, int>())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Prestation"/> class.
+        /// </summary>
+        /// <param name="libelle">Libellé.</param>
+        /// <param name="dateHeureSoin">Date du soin.</param>
+        /// <param name="intervenant">Intervenant.</param>
+        /// <param name="cotation">Cotation.</param>
+        /// <param name="nombre">Nombre.</param>
+        public Prestation(string libelle, DateTime dateHeureSoin, Intervenant intervenant, Cotation cotation, int nombre)
+            : this(libelle, dateHeureSoin, intervenant)
+        {
+            this.cotations.Add(cotation, nombre);
         }
 
         /// <summary>
@@ -57,6 +85,11 @@ namespace CabinetMedical.ClassesMetier
         public Intervenant Intervenant { get => this.intervenant; set => this.intervenant = value; }
 
         /// <summary>
+        /// Gets la nomenclature de la prestation.
+        /// </summary>
+        public Dictionary<Cotation, int> Cotations { get => this.cotations; }
+
+        /// <summary>
         /// Méthode qui compare la date de 2 prestations.
         /// </summary>
         /// <param name="p1">Prestation 1.</param>
@@ -65,6 +98,31 @@ namespace CabinetMedical.ClassesMetier
         public static int CompareTo(Prestation p1, Prestation p2)
         {
             return DateTime.Compare(p1.DateHeureSoin.Date, p2.DateHeureSoin.Date);
+        }
+
+        /// <summary>
+        /// Récupère le prix total de la prestation.
+        /// </summary>
+        /// <returns>Prix.</returns>
+        public double GetPrixPrestation()
+        {
+            double prixCumul = 0;
+            foreach (KeyValuePair<Cotation, int> cotation in this.cotations)
+            {
+                prixCumul += cotation.Key.Indice * cotation.Value;
+            }
+
+            return prixCumul;
+        }
+
+        /// <summary>
+        /// Ajoute une charge à la prestation.
+        /// </summary>
+        /// <param name="cotation">Element d'une nomenclature.</param>
+        /// <param name="nombre">Nombre d'element.</param>
+        public void AjoutePrix(Cotation cotation, int nombre)
+        {
+            this.cotations.Add(cotation, nombre);
         }
     }
 }
